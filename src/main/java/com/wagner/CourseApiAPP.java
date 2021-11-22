7d87ef6a4d53432b37e22abcac618ac5dfbbe488
wagnerkaba <bom.link+github@gmail.com>
2021-11-22 18:29:42 -0300

    Primeiro commit. Criação do pom.xml e da classe principal CourseApiAPP https://www.youtube.com/watch?v=E7_a-kB46LU

---
+++ src/main/java/com/wagner/CourseApiAPP.java
@@ -0,0 +1,14 @@
+package com.wagner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiAPP {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CourseApiAPP.class, args);
+
+    }
+}
